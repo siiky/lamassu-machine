4dd301b2f5dae49ec471ab73f6fd009e589ed9dc
siiky <github-siiky@net-c.cat>
2024-09-23 11:21:30 +0100

    feat: add T&C schema

--- migrations/1726832062427-initial.sql
+++ migrations/1726832062427-initial.sql
@@ -4,7 +4,6 @@ CREATE TABLE versions (
   current INTEGER NOT NULL,
   latest  INTEGER NOT NULL
 );
-
 INSERT INTO versions (current, latest) VALUES (0, 0);
 
 CREATE TABLE configs (
@@ -13,7 +12,25 @@ CREATE TABLE configs (
   data      TEXT NOT NULL
 );
 
+CREATE TABLE terms (
+  version INTEGER NOT NULL,
+  hash    TEXT,
+  active INTEGER NOT NULL
+);
+
+CREATE TABLE terms_by_hash (
+  hash    TEXT NOT NULL,
+  title   TEXT NOT NULL,
+  text    TEXT NOT NULL,
+  accept  TEXT NOT NULL,
+  cancel  TEXT NOT NULL,
+  tcphoto INTEGER NOT NULL,
+  delay   INTEGER NOT NULL
+);
+
 -- Down
 
+DROP TABLE terms_by_hash;
+DROP TABLE terms;
 DROP TABLE configs;
 DROP TABLE versions;
